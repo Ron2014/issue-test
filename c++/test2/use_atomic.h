84611bbe5c86658e493298b367bab9ed41f4e21d
Ron2014 <dev_ron@163.com>
2020-01-02 16:39:39 +0800

    atomic way

--- c++/test2/use_atomic.h
+++ c++/test2/use_atomic.h
@@ -1,7 +1,8 @@
 #include <atomic>
 using namespace std;
 // atomic<int> i = 0;
-std::atomic_int i{0};
+// std::atomic_int i{0};
+std::atomic<int> i{0};
 
 void iplusplus() {
     int c = 10000000;  //循环次数
