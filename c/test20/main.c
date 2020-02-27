78df15ba923ae60dc0776ebba325f5e61cb8e69f
ron <dev_ron@163.com>
2020-02-27 14:37:03 +0800

    test string in macro

--- c/test20/main.c
+++ c/test20/main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <string.h>
+
+#define STR_TEST "string"
 
 int main(int argc, char *argv[]) {
     printf("size: pointer %zd\n", sizeof(void*));
@@ -6,5 +9,6 @@ int main(int argc, char *argv[]) {
     printf("size: int %zd\n", sizeof(int));
     printf("size: long unsigned int %zd\n", sizeof(long unsigned int));
     printf("size: size_t %zd\n", sizeof(size_t));
+    printf("size: const char* in macro %zd, len %zd\n", sizeof(STR_TEST), strlen(STR_TEST));
     return 0;
 }
