275b15295cdac7437e9ae93db653b35b05dd022d
Ron2014 <dev_ron@163.com>
2021-04-02 16:11:41 +0800

    files

--- c++/test40/test.cc
+++ c++/test40/test.cc
@@ -6,6 +6,21 @@
 #include <iostream>
 using namespace std;
 
+typedef unsigned char BOOL;
+
+enum AddTeam_Code
+{
+    AddTeam_Success = 0,      //成功
+    AddTeam_DisAgree = 3,     //不同意
+    AddTeam_TargetInTeam = 4, //对方正在队伍中
+    AddTeam_Full = 10005,
+    AddTeam_NoTeam = 5,
+    AddTeam_InMatching = 9,        //队伍正在匹配中了
+    AddTeam_TargetInMatching = 10, //对方正在匹配中了
+    AddTeam_InGame = 10009,        //队伍正在游戏中了
+    AddTeam_Exception = 10010,
+};
+
 int main(int argc, char *argv[])
 {
     int a = 2147483647;
@@ -14,6 +29,7 @@ int main(int argc, char *argv[])
 
     cout << sizeof(BOOL) << endl;
     cout << sizeof(bool) << endl;
+    cout << sizeof(AddTeam_Code) << endl;
 
     return 0;
 }
